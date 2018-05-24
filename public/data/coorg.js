b95b25b8859262e85478a398f2691dba98144c15
gautam <g.behera@cogentec.in>
2018-05-24 11:35:34 +0530

    Fixed few bugs

--- public/data/coorg.js
+++ public/data/coorg.js
@@ -2,35 +2,35 @@ var coorgData = [
   {
     "location": "View Point Park",
     "description": "Scenic overlook providing panoramic views of mountains, valley, morning fog & sunsets.",
-    "tripTime": "26 MAY,2018 11:00 AM",
+    "tripTime": "26 MAY,2018 10:30 AM",
     "openHours": "6:00 AM - 7:00 PM",
     "visitorStayTime": 45
   },
   {
     "location": "Raja's Seat",
     "description": "Raja's Seat is a seasonal garden of flowers and artificial fountains. It is one of the most important tourist spots in Madikeri of Coorg District. It is 270 km away from Bangalore, the capital of Karnataka.",
-    "tripTime": "26 MAY,2018 12:00 AM",
+    "tripTime": "26 MAY,2018 11:30 AM",
     "openHours": "6:00 AM - 8:00 PM",
     "visitorStayTime": 60
   },
   {
     "location": "Government Museum",
     "description": "Historical place museum in Madikeri, Karnataka",
-    "tripTime": "26 MAY,2018 1:00 AM",
+    "tripTime": "26 MAY,2018 12:30 PM",
     "openHours": "10:00 AM - 5:30 PM",
     "visitorStayTime": 40
   },
   {
     "location": "Madikeri Fort",
     "description": "Madikeri Fort also called Mercara Fort was first founded by Mudduraja in the second half of the 17th century. He also built a palace inside the fort. It was eventually rebuilt and restructured in granite by Tipu Sultan who named the site as Jaffarabad. Mercara Fort is among the many forts, build or rebuild by Tipu Sultan.",
-    "tripTime": "26 MAY,2018 2:30 AM",
+    "tripTime": "26 MAY,2018 2:00 PM",
     "openHours": "9:00 AM - 5:30 PM",
     "visitorStayTime": 55
   },
   {
     "location": "Abbey Falls Madikeri",
     "description": "Abbey Falls is in Kodagu, in the Western Ghats in Karnataka. It is located 8 km from the Madikeri, 122 km from Mysore, 144 km from Mangalore and 268 km from Bangalore. The river is part of the early reaches of the river Kaveri",
-    "tripTime": "26 MAY,2018 4:00 AM",
+    "tripTime": "26 MAY,2018 3:30 PM",
     "openHours": "7:00 AM - 6:00 PM",
     "visitorStayTime": 35
   },
@@ -42,7 +42,7 @@ var coorgData = [
   {
     "location": "Tadiandamol Hills",
     "description": "Tourist attraction in Yevakapadi, India",
-    "tripTime": "27 MAY,2018 1:00 AM",
+    "tripTime": "27 MAY,2018 1:00 PM",
     "openHours": "6:00 AM - 5:00 PM"
   },
   {
@@ -69,7 +69,7 @@ var coorgData = [
   {
     "location": "Golden Temple",
     "description": "The Namdroling Nyingmapa Monastery is the largest teaching center of the Nyingma lineage of Tibetan Buddhism in the world.",
-    "tripTime": "28 MAY,2018 12:00 AM",
+    "tripTime": "28 MAY,2018 12:00 PM",
     "openHours": "8:00 AM - 7:10 PM",
     "visitorStayTime": 60
   }
--- public/scripts/script.js
+++ public/scripts/script.js
@@ -2,3 +2,12 @@ let source = document.getElementById("tripLocationsTemplate").innerHTML;
 let template = Handlebars.compile(source);
 let html = template(coorgData);
 document.getElementById('tripLocations').innerHTML  = html;
+
+if('serviceWorker' in navigator){
+	try{
+		navigator.serviceWorker.register('sw.js');
+		console.log('SW Registered');
+	}catch(error){
+		console.log('SW reg failed');
+	}
+}
