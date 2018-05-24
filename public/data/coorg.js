ef659e1738523f565cfd33ff6967b333b4030dbf
gautam <g.behera@cogentec.in>
2018-05-24 17:19:58 +0530

    Weather IMPL

--- public/data/coorg.js
+++ public/data/coorg.js
@@ -2,75 +2,97 @@ var coorgData = [
   {
     "location": "View Point Park",
     "description": "Scenic overlook providing panoramic views of mountains, valley, morning fog & sunsets.",
-    "tripTime": "26 MAY,2018 10:30 AM",
+    "tripTime": "26 MAY,2018 11:00 AM",
     "openHours": "6:00 AM - 7:00 PM",
-    "visitorStayTime": 45
+    "visitorStayTime": 45,
+    "lat": 12.4138971,
+    "long": 75.667468
   },
   {
     "location": "Raja's Seat",
     "description": "Raja's Seat is a seasonal garden of flowers and artificial fountains. It is one of the most important tourist spots in Madikeri of Coorg District. It is 270 km away from Bangalore, the capital of Karnataka.",
-    "tripTime": "26 MAY,2018 11:30 AM",
+    "tripTime": "26 MAY,2018 12:00 AM",
     "openHours": "6:00 AM - 8:00 PM",
-    "visitorStayTime": 60
+    "visitorStayTime": 60,
+    "lat": 12.4141634,
+    "long": 75.7347679
   },
   {
     "location": "Government Museum",
     "description": "Historical place museum in Madikeri, Karnataka",
-    "tripTime": "26 MAY,2018 12:30 PM",
+    "tripTime": "26 MAY,2018 1:00 AM",
     "openHours": "10:00 AM - 5:30 PM",
-    "visitorStayTime": 40
+    "visitorStayTime": 40,
+    "lat": 12.4203611,
+    "long": 75.7370414
   },
   {
     "location": "Madikeri Fort",
     "description": "Madikeri Fort also called Mercara Fort was first founded by Mudduraja in the second half of the 17th century. He also built a palace inside the fort. It was eventually rebuilt and restructured in granite by Tipu Sultan who named the site as Jaffarabad. Mercara Fort is among the many forts, build or rebuild by Tipu Sultan.",
-    "tripTime": "26 MAY,2018 2:00 PM",
+    "tripTime": "26 MAY,2018 2:30 AM",
     "openHours": "9:00 AM - 5:30 PM",
-    "visitorStayTime": 55
+    "visitorStayTime": 55,
+    "lat": 12.4208223,
+    "long": 75.7368469
   },
   {
     "location": "Abbey Falls Madikeri",
     "description": "Abbey Falls is in Kodagu, in the Western Ghats in Karnataka. It is located 8 km from the Madikeri, 122 km from Mysore, 144 km from Mangalore and 268 km from Bangalore. The river is part of the early reaches of the river Kaveri",
-    "tripTime": "26 MAY,2018 3:30 PM",
+    "tripTime": "26 MAY,2018 4:00 AM",
     "openHours": "7:00 AM - 6:00 PM",
-    "visitorStayTime": 35
+    "visitorStayTime": 35,
+    "lat": 12.4570611,
+    "long": 75.7142022
   },
   {
     "location": "Talakaveri Greater National Park",
     "description": "Panathoor Bhagamandala Inter State Rd, Karike, Karnataka 571247",
-    "tripTime": "27 MAY,2018 8:00 AM"
+    "tripTime": "27 MAY,2018 8:00 AM",
+    "lat": 12.4199377,
+    "long": 75.4672398
   },
   {
     "location": "Tadiandamol Hills",
     "description": "Tourist attraction in Yevakapadi, India",
-    "tripTime": "27 MAY,2018 1:00 PM",
-    "openHours": "6:00 AM - 5:00 PM"
+    "tripTime": "27 MAY,2018 1:00 AM",
+    "openHours": "6:00 AM - 5:00 PM",
+    "lat": 12.2175615,
+    "long": 75.6067913
   },
   {
     "location": "Iruppu Falls",
     "description": "The Irupu Falls is located in the Brahmagiri Range in the Kodagu district of Karnataka, India, bordering the Wayanad district of Kerala",
     "tripTime": "28 MAY,2018 7:30 AM",
     "openHours": "7:15 AM - 5:00 PM",
-    "visitorStayTime": 60
+    "visitorStayTime": 60,
+    "lat": 11.9655703,
+    "long": 75.9815072
   },
   {
     "location": "Dubara Elephant Camp (Optional)",
     "description": "Jungle camp offering elephant bathing, feeding & riding experiences & rafting on the Kaveri River.",
     "tripTime": "28 MAY,2018 10:00 AM",
     "openHours": "8:30 AM - 11:00 AM",
-    "visitorStayTime": 90
+    "visitorStayTime": 90,
+    "lat": 12.3705782,
+    "long": 75.9032214
   },
   {
     "location": "Nisargadhama",
     "description": "Kaveri Nisargadhama is an island formed by river Kaveri near Kushalnagar in the district of Kodagu, Karnataka, India.",
     "tripTime": "28 MAY,2018 10:00 AM",
     "openHours": "9:00 AM - 5:45 PM",
-    "visitorStayTime": 60
+    "visitorStayTime": 60,
+    "lat": 12.4420349,
+    "long": 75.9308267
   },
   {
-    "location": "Golden Temple",
+    "location": "Golden Temple (Optional)",
     "description": "The Namdroling Nyingmapa Monastery is the largest teaching center of the Nyingma lineage of Tibetan Buddhism in the world.",
-    "tripTime": "28 MAY,2018 12:00 PM",
+    "tripTime": "28 MAY,2018 12:00 AM",
     "openHours": "8:00 AM - 7:10 PM",
-    "visitorStayTime": 60
+    "visitorStayTime": 60,
+    "lat": 12.4303871,
+    "long": 75.9653091
   }
-]
+];
--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,13 +1,32 @@
-let source = document.getElementById("tripLocationsTemplate").innerHTML;
-let template = Handlebars.compile(source);
-let html = template(coorgData);
-document.getElementById('tripLocations').innerHTML  = html;
+let fetchWeatherData = function (position) {
+	if (position < coorgData.length) {
+		fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + coorgData[position].lat + '&lon=' +
+			coorgData[position].long + '&APPID=92ea4ed22a08e46a6efa3f2e70bdcafc')
+			.then(function (response) {
+				return response.json();
+			})
+			.then(function (weather) {
+				coorgData[position].Temperature = (weather.main.temp - 273.15).toFixed(2) + "<sup>o</sup>C";
+				coorgData[position].Weather = weather.weather[0].description;
+				fetchWeatherData(position + 1);
+			});
+	} else {
+		let source = document.getElementById("tripLocationsTemplate").innerHTML;
+		let template = Handlebars.compile(source);
+		let html = template(coorgData);
+		document.getElementById('tripLocations').innerHTML = html;
+	}
+};
 
-if('serviceWorker' in navigator){
-	try{
+fetchWeatherData(0);
+
+
+
+if ('serviceWorker' in navigator) {
+	try {
 		navigator.serviceWorker.register('sw.js');
 		console.log('SW Registered');
-	}catch(error){
+	} catch (error) {
 		console.log('SW reg failed');
 	}
 }
